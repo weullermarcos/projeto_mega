511d5191d40c57343a2e81df4068f16a39202c4f
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2020-03-17 17:43:38 -0300

    subindooo

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
 	echo "Ola mundo";
+	
+	$numero1 = 1; $numero2 = 2;
+	$numero3 = 3; $numero4 = 4;
+	$numero5 = 5; $numero6 = 6;
+	
+	/*
+	echo "</br>".$numero1;
+	echo "</br>".$numero2;
+	echo "</br>".$numero3;
+	echo "</br>".$numero4;
+	echo "</br>".$numero5;
+	echo "</br>".$numero6;
+	*/
+	
+	echo"</br></br>";
+	echo "Combinação 1: - [ ".$numero1." | ".$numero2." | ".$numero3." | ".$numero4." | ".$numero5." | ".$numero6." ]";
 
 ?>
