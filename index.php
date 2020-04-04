c879d433db60d5eb45daf43040f99d70d97892fb
System Administrator <root@MBP-de-Weuller.lan>
2020-04-03 21:53:06 -0300

    gerando todos os numeros possiveis

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-	$numero1 = 1; $numero2 = 1;
-	$numero3 = 1; $numero4 = 1;
-	$numero5 = 1; $numero6 = 1;
+	$numero1 = 1; $numero2 = 1; $numero3 = 1; 
+	$numero4 = 1; $numero5 = 1; $numero6 = 1;
 	
 	$combinacao = 1;
 	
@@ -13,10 +12,20 @@
 		for($i2 = 0; $i2 < 60; $i2++){
 			for($i3 = 0; $i3 < 60; $i3++){
 				for($i4 = 0; $i4 < 60; $i4++){
+					for($i5 = 0; $i5 < 60; $i5++){
+						for($i6 = 0; $i6 < 60; $i6++){
+
+							echo "</br>Combinação ".$combinacao.": - [ ".$numero1." | ".$numero2." | ".$numero3." | ".$numero4." | ".$numero5." | ".$numero6." ]";
 					
-					echo "</br>Combinação ".$combinacao.": - [ ".$numero1." | ".$numero2." | ".$numero3." | ".$numero4." | ".$numero5." | ".$numero6." ]";
-					
-					$combinacao++;
+							$combinacao++;
+							$numero6++;	
+						}
+
+						$numero6 = 1;
+						$numero5++;	
+					}
+
+					$numero5 = 1;
 					$numero4++;	
 				}
 				
@@ -33,3 +42,8 @@
 	}
 
 ?>
+
+
+
+
+
